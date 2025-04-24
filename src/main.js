d05b32b1ed73128799db84f7ca2eef353ebe99b8
KanishkChhajed <kanishk.chhajed@gmail.com>
2025-04-24 10:51:25 +0530

    Include .gitignore file and create authentication in main.js using octokit

--- src/main.js
+++ src/main.js
@@ -1,10 +1,23 @@
+import {Ocktokit} from "octokit"
+
+
 const core = require('@actions/core')
+
 
 function generate(){
 
     const token = core.getInput('token')
     
     try{
+        // Authentication process
+        try{
+            const octokit = new Octokit({auth : token})
+            console.log(octokit)
+        }catch{
+            console.log("Authentication process failed...")
+        }
+        
+
         console.log(token)
         console.log("Getting token successfully")
     }

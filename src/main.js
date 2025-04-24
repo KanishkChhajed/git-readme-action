a69c63415ad3747eb1b034afdbfd58451cfab10b
KanishkChhajed <kanishk.chhajed@gmail.com>
2025-04-24 09:17:04 +0530

    Update all files code

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 const core = require('@actions/core')
 
+function generate(){
 
-const token = core.getInput('token')
-
-try{
-   console.log(token)
+    const token = core.getInput('token')
+    
+    try{
+        console.log(token)
+        console.log("Getting token successfully")
+    }
+    catch {
+        console.log("Error")
+    }
 }
-catch {
-    console.log("Error")
-}
 
-run();
+generate();

ee9a38a02c3c70af732d9fa63bc69f91c2009e0b
KanishkChhajed <kanishk.chhajed@gmail.com>
2025-04-25 10:08:42 +0530

    Removing type: module from package.json

--- src/main.js
+++ src/main.js
@@ -1,51 +1,38 @@
-// import { Octokit } from "@octokit/action"
-// import github  from "@actions/github"
-// import  core from '@actions/core'
+const { Octokit } = require("@octokit/action")
+const github  = require("@actions/github")
+const  core  =  require('@actions/core')
 
 
-// async function  generate(){
+async function  generate(){
 
-//     const token = core.getInput('token')
+    const token = core.getInput('token')
     
-//     try{
-//         // Authentication process
-//         const octokit = new Octokit({auth : token})
-//         const context = github.context
-//         try{
-//             const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+    try{
+        // Authentication process
+        const octokit = new Octokit({auth : token})
+        const context = github.context
+        try{
+            const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
 
-//             const {data : reposData} = await octokit.rest.repos.get({
-//                 owner,
-//                 repo
-//             })
+            const {data : reposData} = await octokit.rest.repos.get({
+                owner,
+                repo
+            })
 
-//             console.log(`Repo: ${reposData.repo}, Owner: ${reposData.owner}`);
-//             console.log("Successfully authenticated and fetched repo.");
-//         }catch (error){
-//             console.log("Authentication process failed...")
-//             console.log (error)
-//         }
+            console.log(`Repo: ${reposData.repo}, Owner: ${reposData.owner}`);
+            console.log("Successfully authenticated and fetched repo.");
+        }catch (error){
+            console.log("Authentication process failed...")
+            console.log (error)
+        }
         
 
-//         // console.log(token)
-//         console.log("Getting token successfully")
-//     }
-//     catch  (error){
-//         console.log(error)
-//     }
-// }
+        // console.log(token)
+        console.log("Getting token successfully")
+    }
+    catch  (error){
+        console.log(error)
+    }
+}
 
-// generate();
-
-import { Octokit } from "@octokit/action";
-
-const octokit = new Octokit();
-const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
-
-// See https://developer.github.com/v3/issues/#create-an-issue
-const { data } = await octokit.request("POST /repos/{owner}/{repo}/issues", {
-  owner,
-  repo,
-  title: "My test issue",
-});
-console.log("Issue created: %s", data.html_url);
+generate();

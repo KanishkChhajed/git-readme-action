c671b9bd238bf8f7671f73c69f43f4b578b1639b
KanishkChhajed <kanishk.chhajed@gmail.com>
2025-04-26 15:14:17 +0530

    Convert log -> console.log in dependences.js file

--- src/dependences.js
+++ src/dependences.js
@@ -1,4 +1,4 @@
 const {execSync} = require('node:child_process')
 
 execSync(`npm install @actions/core octokit @actions/github`)
-log("Dependencies installation complete")
+console.log("Dependencies installation complete")

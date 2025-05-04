9cfe730ae0869cbcd0f59d9da901d0760db2410d
KanishkChhajed <kanishk.chhajed@gmail.com>
2025-05-04 10:22:58 +0530

    Testing isJavaScript.js file

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ const ejs = require('ejs')
 const fs = require('fs')
 const path = require('path')
 const { execSync } = require("child_process")
-const detect_dependencies = require('/src/isJavaScript.js')
+const detect_dependencies = require('./isJavaScript.js')
 
 const template_path = './temp/README_template3.ejs'
 const output_path = 'README.md'

14e6fcb0760d8c5e86f79b49d98cf7e60f760b6b
KanishkChhajed <kanishk.chhajed@gmail.com>
2025-05-06 08:46:56 +0530

    Updating more dependencies in depenences.js file

--- src/dependences.js
+++ src/dependences.js
@@ -1,4 +1,4 @@
 import {execSync} from 'node:child_process'
 
-execSync(`npm install @actions/core octokit @actions/github`)
+execSync(`npm install @actions/core octokit @actions/github toml child_process js-yaml xml2js`)
 console.log("Dependencies installation complete")

e3d3611f64720b391aa5e6cadbf171ba64c56a6b
KanishkChhajed <kanishk.chhajed@gmail.com>
2025-05-08 02:04:43 +0530

    Adding error handling and recursive code in isJavaScript.js file

--- src/isJavaScript.js
+++ src/isJavaScript.js
@@ -4,93 +4,171 @@ import path from 'path'
 
 let techstack_Set = new Set();
 
+const JavaScript = [
+  "package.json",
+  "package-lock.json",
+  "yarn.lock",
+  "pnpm-lock.yaml",
+];
+
 function isInclude(allFiles, dependencyPackage) {
-  if (!allFiles || !dependencyPackage) return [];
-  return dependencyPackage.filter((file) =>
-    allFiles.includes(path.basename(file))
+  try{
+
+    if (!allFiles || !dependencyPackage) return [];
+    return dependencyPackage.filter((file) =>
+      allFiles.includes(path.basename(file))
   );
+}catch(err){
+  console.error(`Error in isInclude function:`, err.message);
+    return [];
+}
+}
+
+
+function JavaScript_dir(dir = process.cwd()){
+          // const dir = process.cwd()
+           try{
+            const folder = fs.readdirSync(dir)
+            const allFiles = []
+            for(const file of folder){
+              const Path = path.join(dir,file)
+              const Pathstat = fs.statSync(Path)
+              if(Pathstat.isDirectory()){
+                const subDeps = JavaScript_dir(Path)
+                if (Array.isArray(subDeps)) {
+                  allFiles.push(...subDeps)
+                }
+                console.log(`Successfully recursion on path:${Path}`)
+              }else if(Pathstat.isFile()){
+                allFiles.push(Path)
+                console.log(`Successfully push path on allFiles:${Path}`)
+              } 
+            }
+            const check =  isInclude(allFiles,JavaScript)
+            return check;  
+          }catch(err){
+            console.error(`Error occured in JavaScript_dir function`,err.message)
+            return []
+          }
 }
 
 export async function JavaScript_dependencies() {
-  const workSpace = process.env.GITHUB_WORKSPACE;
-  const files = fs.readdirSync(workSpace);
+  // const workSpace = process.env.GITHUB_WORKSPACE;
+  // const files = fs.readdirSync(workSpace);
   // const lang = process.env.GITHUB_L;
 
   // Identify which language is used in the project
 
-  const JavaScript = [
-    "package.json",
-    "package-lock.json",
-    "yarn.lock",
-    "pnpm-lock.yaml",
-  ];
-  let isJavaScript = isInclude(files, JavaScript);
+  
+  // let isJavaScript = isInclude(files, JavaScript);
+  try{
 
-  for (const file of isJavaScript) {
-    if (file === "package.json") {
-      // const pkg = JSON.parse(fs.readFileSync(file, "utf-8"));
-      let pkg;
-      try {
-        pkg = await JSON.parse(fs.readFileSync(path.join(workSpace, file), "utf-8"));
-      } catch (e) {
+    const check = JavaScript_dir()
+    
+  if(check && check.length){
+    
+    for (const file of check) {
+      if (file === "package.json") {
+        // const pkg = JSON.parse(fs.readFileSync(file, "utf-8"));
+        let pkg;
+        try {
+          pkg = await JSON.parse(fs.readFileSync(file, "utf-8"));
+        } catch (e) {
         console.error(`Error parsing ${file}: ${e.message}`);
         continue;
       }
-      let dependencyArray = Object.keys(pkg.dependencies || {});
-      let devDependencyArray = Object.keys(pkg.devDependencies || {});
-      for (const dep of dependencyArray) {
-        techstack_Set.add(dep.split(":")[0]);
-      }
-      for (const dep of devDependencyArray) {
-        techstack_Set.add(dep.split(":")[0]);
+      try{
+
+        let dependencyArray = Object.keys(pkg.dependencies || {});
+        let devDependencyArray = Object.keys(pkg.devDependencies || {});
+        for (const dep of dependencyArray) {
+          techstack_Set.add(dep.split(":")[0]);
+        }
+        for (const dep of devDependencyArray) {
+          techstack_Set.add(dep.split(":")[0]);
+        }
+      }catch(err){
+        console.error(`Error occured ${fileName}:`,err.message())
       }
     } else if (file === "package-lock.json") {
-      const pkg = JSON.parse(
-        fs.readFileSync(path.join(workSpace, file), "utf-8")
-      );
-      const dependencies =
+      let pkg
+      try{
+        pkg = JSON.parse(
+          fs.readFileSync(file, "utf-8")
+        );
+      }catch(err){
+        console.error(`Error parsing ${file}: ${err.message}`)
+      }
+      try{
+
+        const dependencies =
         pkg.dependencies || pkg.packages?.[""]?.dependencies || {};
-      for (const dep of Object.keys(dependencies)) {
-        techstack_Set.add(dep);
+        for (const dep of Object.keys(dependencies)) {
+          techstack_Set.add(dep);
+        }
+      }catch(err){
+        console.error(`Error occured ${fileName}:`,err.message())
       }
     } else if (file === "yarn.lock") {
-      const output = execSync(`yarn list`, {
-        encoding: "utf-8",
-        timeout: 5000,
-      });
-      const lines = output.trim().split("\n");
-      for (const line of lines) {
-        let parseLine;
-        try {
-          parseLine = JSON.parse(line);
-        } catch (e) {
-          console.error(`Error parsing ${file}: ${e.message}`);
-          continue;
-        }
-        if (parseLine.type === "tree") {
-          for (const dep of parseLine.data.trees) {
-            const depName = dep.name.split("@")[0];
-            techstack_Set.add(depName);
+      let output
+      try{
+        output = execSync(`yarn list`, {
+          encoding: "utf-8",
+          timeout: 5000,
+        });
+      }catch(err){
+        console.error(`Error executing ${file}: ${err.message}`)
+      }
+      try{
+
+        const lines = output.trim().split("\n");
+        for (const line of lines) {
+          let parseLine;
+          try {
+            parseLine = JSON.parse(line);
+          } catch (e) {
+            console.error(`Error parsing ${file}: ${e.message}`);
+            continue;
+          }
+          if (parseLine.type === "tree") {
+            for (const dep of parseLine.data.trees) {
+              const depName = dep.name.split("@")[0];
+              techstack_Set.add(depName);
+            }
           }
         }
+      }catch(err){
+        console.error(`Error occured ${fileName}:`,err.message())
       }
     } else if (file === "pnpm-lock.yaml") {
-      const pkg = JSON.parse(
-        fs.readFileSync(path.join(workSpace, file)),
-        "utf-8"
-      );
-      let dependencyArray = Object.keys(pkg.dependencies || {});
-      let devDependencyArray = Object.keys(pkg.devDependencies || {});
-      for (const dep of dependencyArray) {
-        techstack_Set.add(dep.split(":")[0]);
+      let pkg
+      try{
+        pkg = JSON.parse(fs.readFileSync(file,"utf-8"));
+      }catch(err){
+        console.error(`Error parsing ${file}: ${err.message}`)
       }
-      for (const dep of devDependencyArray) {
-        techstack_Set.add(dep.split(":")[0]);
+      try{
+
+        let dependencyArray = Object.keys(pkg.dependencies || {});
+        let devDependencyArray = Object.keys(pkg.devDependencies || {});
+        for (const dep of dependencyArray) {
+          techstack_Set.add(dep.split(":")[0]);
+        }
+        for (const dep of devDependencyArray) {
+          techstack_Set.add(dep.split(":")[0]);
+        }
+      }catch(err){
+        console.error(`Error occured ${fileName}:`,err.message())
       }
     } else {
-        console.log("No common package dependency file found....");
-        return [];
+      techstack_Set.clear();
+      console.log("No common package dependency file found....");
+      return [];
     }
   }
   return Array.from(techstack_Set).filter(Boolean);
+}
+}catch(err){
+  console.error(`Error occured:`,err.message)
+}
 }

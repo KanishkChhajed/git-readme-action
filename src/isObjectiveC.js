04cf3ac3d0973498175b37e765efa98a37318d10
KanishkChhajed <kanishk.chhajed@gmail.com>
2025-05-08 09:23:18 +0530

    Convert into type: module syntax in ObjectiveC and Ruby file

--- src/isObjectiveC.js
+++ src/isObjectiveC.js
@@ -1,6 +1,6 @@
-const fs = require("fs");
-const path = require("path");
-const yaml = require("js-yaml");
+import fs from "fs";
+import path from "path";
+import yaml from "js-yaml";
 
 const Objective_C = ["Podfile", "Podfile.lock"];
 

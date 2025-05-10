6bf44a1b798464c03f529f4a8bbba437e39dff6c
KanishkChhajed <kanishk.chhajed@gmail.com>
2025-05-10 11:50:04 +0530

    Adding all installation process in dependencies.js file

--- src/dependences.js
+++ src/dependences.js
@@ -1,4 +1,6 @@
 import {execSync} from 'node:child_process'
 
+execSync(`npm ci`)
+execSync(`npm init -y`)
 execSync(`npm install @actions/core octokit @actions/github`)
 console.log("Dependencies installation complete")

176cadc676669bb5569f1e9ebaab961ca73c006e
KanishkChhajed <kanishk.chhajed@gmail.com>
2025-05-10 11:52:53 +0530

    Removing npm install from dependencies.js file

--- src/dependences.js
+++ src/dependences.js
@@ -1,6 +1,4 @@
 import {execSync} from 'node:child_process'
 
-execSync(`npm install`)
-execSync(`npm init -y`)
 execSync(`npm install @actions/core octokit @actions/github`)
 console.log("Dependencies installation complete")

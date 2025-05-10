59b688b17366f4d4c6ce3c7876cd6c7389afd69f
KanishkChhajed <kanishk.chhajed@gmail.com>
2025-05-10 11:51:32 +0530

    Replacing npm ci -> npm install in dependencies.js file

--- src/dependences.js
+++ src/dependences.js
@@ -1,6 +1,6 @@
 import {execSync} from 'node:child_process'
 
-execSync(`npm ci`)
+execSync(`npm install`)
 execSync(`npm init -y`)
 execSync(`npm install @actions/core octokit @actions/github`)
 console.log("Dependencies installation complete")
